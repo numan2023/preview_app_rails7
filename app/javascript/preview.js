962e0fdfae11388cb2b83302c2cbe7a92e7c36e0
numan2023 <fullmetal0212@gmail.com>
2023-10-10 16:50:15 +0900

    preview_implement

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -13,10 +13,20 @@ document.addEventListener('turbo:load', function(){
   const fileField = document.querySelector('input[type="file"][name="post[image]"]');
   //input要素で値の変化が起きた際に呼び出される関数
   fileField.addEventListener('change', function(e){
-    console.log("input要素で値の変化が起きました");
-    console.log(e.target.files[0]);
     const file = e.target.files[0];
     const blob = window.URL.createObjectURL(file);
-    console.log(blob);
+
+    // 画像を表示するためのdiv要素を生成
+    const previewWrapper = document.createElement('div');
+    previewWrapper.setAttribute('class', 'preview');
+
+    // 表示する画像を生成
+    const previewImage = document.createElement('img');
+    previewImage.setAttribute('class', 'preview-image');
+    previewImage.setAttribute('src', blob);
+
+    // 生成したHTMLの要素をブラウザに表示させる
+    previewWrapper.appendChild(previewImage);
+    previewList.appendChild(previewWrapper);
   });
 });
